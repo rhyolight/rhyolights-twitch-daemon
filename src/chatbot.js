8bd110fae0d727ff25a9c4777feed264cbbd1fc9
Matthew Taylor <matt@numenta.org>
2019-05-29 09:51:41 -0700

    Added sound command

--- src/chatbot.js
+++ src/chatbot.js
@@ -1,93 +1,104 @@
 const tmi = require('tmi.js');
 const parseDuration = require('parse-duration')
+const path = require('path')
+const play = require('play')
 
 // function stringSubTag(strings, argsExpr) {
 // }
 
 class Chatbot {
 
-    constructor(name, channel, oath, commands) {
-        this.name = name
-        this.channel = channel
-        this.oath = oath
-        this.commands = commands
+  constructor(name, channel, oath, commands) {
+    this.name = name
+    this.channel = channel
+    this.oath = oath
+    this.commands = commands
+  }
+
+  start() {
+    let me = this
+    // Create a client with our options
+    let client = this.client = new tmi.client({
+      identity: {
+        username: this.name,
+        password: this.oath,
+      },
+      channels: [
+        this.channel
+      ]
+    });
+    // Register our event handlers (defined below)
+    client.on('message', (target, context, msg, self) => {
+      me.onMessageHandler(target, context, msg, self)
+    });
+    client.on('connected', (addr, port) => {
+      this.onConnectedHandler(addr, port)
+    });
+
+    // Connect to Twitch:
+    client.connect();
+  }
+
+  listCommands(target) {
+    let cmds = Object.keys(this.commands).join(' ')
+    this.client.say(target, `Available commands: ${cmds}`)
+  }
+
+  countdown(target, time) {
+    console.log(time)
+    let duration = parseDuration(time)
+    let remaining = duration
+    this.client.say(target, `Starts in: ${remaining}ms`)
+  }
+
+  playSound(sound) {
+    let soundFile = path.join(__dirname, '../resources/sounds', `${sound}.wav`)
+    console.log(`playing sound from ${soundFile}`)
+    play.sound(soundFile);
+  }
+
+  onConnectedHandler(addr, port) {
+    console.log(`* Connected to ${addr}:${port}`);
+  }
+
+  onMessageHandler(target, context, msg, self) {
+    // Ignore messages from the bot
+    if (self) {
+      return
+    }
+    const args = msg.trim().split(/\s+/)
+    const commandName = args.shift();
+
+    console.log(`Recieved message "${msg}"`)
+    console.log(`command: "${commandName}"`)
+    console.log(`args: ${args}`)
+
+    // Ignore anything not starting with '!'
+    if (!commandName.startsWith('!')) {
+      return
     }
 
-    start() {
-        let me = this
-        // Create a client with our options
-        let client = this.client = new tmi.client({
-            identity: {
-                username: this.name,
-                password: this.oath,
-            },
-            channels: [
-                this.channel
-            ]
-        });
-        // Register our event handlers (defined below)
-        client.on('message', (target, context, msg, self) => {
-            me.onMessageHandler(target, context, msg, self)
-        });
-        client.on('connected', (addr, port) => {
-            this.onConnectedHandler(addr, port)
-        });
-
-        // Connect to Twitch:
-        client.connect();
+    // The '!commands' command is special
+    if (commandName === '!commands') {
+      this.listCommands(target)
+      // The '!countdown' command is special
+    } else if (commandName === '!countdown') {
+      this.countdown(target, args[0])
+      // The '!sound' comamnd is special
+    } else if (commandName === '!sound') {
+      this.playSound(args[0])
+    } else {
+      // If the command is known, let's execute it
+      let cmd = this.commands[commandName];
+      if (cmd) {
+        let reply = cmd.text
+        this.client.say(target, reply)
+        console.log(`* Executed ${commandName} command`);
+      } else {
+        console.log(`* Unknown command ${commandName}`);
+      }
     }
-
-    listCommands(target) {
-        let cmds = Object.keys(this.commands).join(' ')
-        this.client.say(target, `Available commands: ${cmds}`)
-    }
-
-    countdown(target, time) {
-        console.log(time)
-        let duration = parseDuration(time)
-        let remaining = duration
-        this.client.say(target, `Starts in: ${remaining}ms`)
-    }
-
-    onConnectedHandler(addr, port) {
-        console.log(`* Connected to ${addr}:${port}`);
-    }
-
-    onMessageHandler(target, context, msg, self) {
-        // Ignore messages from the bot
-        if (self) {
-            return
-        }
-        const args = msg.trim().split(/\s+/)
-        const commandName = args.shift();
-
-        console.log(`Recieved message "${msg}"`)
-        console.log(`command: "${commandName}"`)
-        console.log(`args: ${args}`)
-
-        // Ignore anything not starting with '!'
-        if (!commandName.startsWith('!')) {
-            return
-        }
-
-        // The '!commands' command is special
-        if (commandName === '!commands') {
-            this.listCommands(target)
-            // The '!countdown' command is special
-        } else if (commandName === '!countdown') {
-            this.countdown(target, args[0])
-        } else {
-            // If the command is known, let's execute it
-            let cmd = this.commands[commandName];
-            if (cmd) {
-                let reply = cmd.text
-                this.client.say(target, reply)
-                console.log(`* Executed ${commandName} command`);
-            } else {
-                console.log(`* Unknown command ${commandName}`);
-            }
-        }
-    }
+  }
 
 }
 
